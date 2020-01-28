784d150af4ef7daf31549631a718c623ed61865f
Arran <arran4@gmail.com>
2020-01-28 15:32:41 +1100

    Major changes

--- gaelogger.go
+++ gaelogger.go
@@ -3,33 +3,67 @@ package gaelogger
 import (
 	"cloud.google.com/go/logging"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"runtime"
 )
 
-type GaeLogger struct {
-	loggingClient *logging.Client
-	logger        *logging.Logger
-	r             *http.Request
+type backLogger interface {
+	logf(severity fmt.Stringer, format string, args ...interface{})
 }
 
-func NewGaeLogger(r *http.Request) *GaeLogger {
+type Logger interface {
+	io.Closer
+	Defaultf(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Noticef(format string, args ...interface{})
+	Warningf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Criticalf(format string, args ...interface{})
+	Alertf(format string, args ...interface{})
+	Emergencyf(format string, args ...interface{})
+}
+
+func NewLogger(r *http.Request) Logger {
 	if len(os.Getenv("GOOGLE_CLOUD_PROJECT")) > 0 {
 		loggingClient, err := logging.NewClient(r.Context(), os.Getenv("GOOGLE_CLOUD_PROJECT"))
 		if err != nil {
 			log.Fatalf("Failed to create client: %v", err)
 		}
 		logger := loggingClient.Logger("request")
-		return &GaeLogger{
-			loggingClient: loggingClient,
-			logger:        logger,
-			r:             r,
+		return &WrapLogger{
+			backLogger: &GaeLogger{
+				loggingClient: loggingClient,
+				logger:        logger,
+				r:             r,
+			},
 		}
 	} else {
-		return &GaeLogger{}
+		return &WrapLogger{
+			backLogger: &StdLogger{
+				logger: log.New(os.Stderr, "", log.Lshortfile|log.LstdFlags),
+			},
+		}
 	}
+}
+
+type StdLogger struct {
+	logger *log.Logger
+}
+
+func (st *StdLogger) logf(severity fmt.Stringer, format string, args ...interface{}) {
+	st.logger.SetPrefix(severity.String())
+	st.logger.Output(4, fmt.Sprintf(format, args...))
+	st.logger.SetPrefix("")
+}
+
+type GaeLogger struct {
+	loggingClient *logging.Client
+	logger        *logging.Logger
+	r             *http.Request
 }
 
 func (gl *GaeLogger) Close() error {
@@ -39,57 +73,58 @@ func (gl *GaeLogger) Close() error {
 	return nil
 }
 
-func (gl *GaeLogger) logf(severity logging.Severity, format string, args ...interface{}) {
-	if gl.logger != nil {
-		_, file, line, ok := runtime.Caller(2)
-		if !ok {
-			file = "???"
-			line = 0
-		}
-		gl.logger.Log(logging.Entry{
-			Severity: severity,
-			Payload:  fmt.Sprintf("%s:%d "+format, append([]interface{}{file, line}, args...)...),
-			HTTPRequest: &logging.HTTPRequest{
-				Request: gl.r,
-			},
-		})
-	} else {
-		log.Printf("Error: "+format, args...)
+func (gl *GaeLogger) logf(severity fmt.Stringer, format string, args ...interface{}) {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		file = "???"
+		line = 0
 	}
+	gl.logger.Log(logging.Entry{
+		Severity: severity.(logging.Severity),
+		Payload:  fmt.Sprintf("%s:%d "+format, append([]interface{}{file, line}, args...)...),
+		HTTPRequest: &logging.HTTPRequest{
+			Request: gl.r,
+		},
+	})
 }
 
-func (gl *GaeLogger) Defaultf(format string, args ...interface{}) {
+type WrapLogger struct {
+	backLogger
+	io.Closer
+}
+
+func (gl WrapLogger) Defaultf(format string, args ...interface{}) {
 	gl.logf(logging.Default, format, args...)
 }
 
-func (gl *GaeLogger) Debugf(format string, args ...interface{}) {
+func (gl *WrapLogger) Debugf(format string, args ...interface{}) {
 	gl.logf(logging.Debug, format, args...)
 }
 
-func (gl *GaeLogger) Infof(format string, args ...interface{}) {
+func (gl *WrapLogger) Infof(format string, args ...interface{}) {
 	gl.logf(logging.Info, format, args...)
 }
 
-func (gl *GaeLogger) Noticef(format string, args ...interface{}) {
+func (gl *WrapLogger) Noticef(format string, args ...interface{}) {
 	gl.logf(logging.Notice, format, args...)
 }
 
-func (gl *GaeLogger) Warningf(format string, args ...interface{}) {
+func (gl *WrapLogger) Warningf(format string, args ...interface{}) {
 	gl.logf(logging.Warning, format, args...)
 }
 
-func (gl *GaeLogger) Errorf(format string, args ...interface{}) {
+func (gl *WrapLogger) Errorf(format string, args ...interface{}) {
 	gl.logf(logging.Error, format, args...)
 }
 
-func (gl *GaeLogger) Criticalf(format string, args ...interface{}) {
+func (gl *WrapLogger) Criticalf(format string, args ...interface{}) {
 	gl.logf(logging.Critical, format, args...)
 }
 
-func (gl *GaeLogger) Alertf(format string, args ...interface{}) {
+func (gl *WrapLogger) Alertf(format string, args ...interface{}) {
 	gl.logf(logging.Alert, format, args...)
 }
 
-func (gl *GaeLogger) Emergencyf(format string, args ...interface{}) {
+func (gl *WrapLogger) Emergencyf(format string, args ...interface{}) {
 	gl.logf(logging.Emergency, format, args...)
 }
